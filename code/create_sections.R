dbe3028ba0ca23411998860cc60df40bd8bac68e
Samuel Woodman <samuel.woodman@gmail.com>
2022-11-01 13:37:41 -0600

    - add flux tower coordinates from drone - add object to specify which flux tower coordinates are used

--- code/create_sections.R
+++ code/create_sections.R
@@ -42,7 +42,8 @@ library(measurements)
 
 # Load data ---------------------------------------------------------------
 
-## Coordiantes from Larry
+## GPS coordinates from field
+### FIXME does not work, coordinates do no match drone imagery
 ft_gps <- tibble(latitude = "50 31 20.9", # N
                longitude = "-113 41 04.47") # W
 
@@ -51,29 +52,35 @@ lat <- conv_unit(ft_gps$latitude, from = "deg_min_sec", to = "dec_deg")
 long <- conv_unit(ft_gps$longitude , from = "deg_min_sec", to = "dec_deg")
 
 ## Create spatial dataframe for flux tower
-ft_coords <- tibble(name = "flux tower",
+ft_coord_gps <- tibble(name = "flux tower",
              latitude = as.numeric(lat),
              longitude = as.numeric(long)) %>%
   st_as_sf(., coords = c("longitude", "latitude"),
            crs = 4326, agr = "constant") %>%
   st_transform(crs = 2956)
 
+## Coordinates manually selected from drone imagery
+
+ft_coord_drone <- st_read(here("data/raw/flux_tower.gpkg"))
+
+ft_pnt <- ft_coord_drone
+
 # Process -----------------------------------------------------------------
 
 ## Create circles increasing 25 m increments around the flux tower
 ### Buffer function create a polygon with the distance argument determining the
 ### radius of the circle.
-ft_25 <- st_buffer(ft_coord, 25)
-ft_50 <- st_buffer(ft_coord, 50)
-ft_75 <- st_buffer(ft_coord, 75)
-ft_100 <- st_buffer(ft_coord, 100)
-ft_125 <- st_buffer(ft_coord, 125)
-ft_150 <- st_buffer(ft_coord, 150)
+ft_25 <- st_buffer(ft_pnt, 25)
+ft_50 <- st_buffer(ft_pnt, 50)
+ft_75 <- st_buffer(ft_pnt, 75)
+ft_100 <- st_buffer(ft_pnt, 100)
+ft_125 <- st_buffer(ft_pnt, 125)
+ft_150 <- st_buffer(ft_pnt, 150)
 
 ## Create a dataframe of coordinates
 point_df <-
   # convert sf point object to x,y coordinates
-  st_coordinates(ft_coord) %>%
+  st_coordinates(ft_pnt) %>%
   # convert to dataframe
   as.data.frame() %>%
   # rename columns
@@ -84,8 +91,8 @@ point_df <-
               # TODO remove points along inner circles since they are unnecessary
               # degrees at 22.5 degree increments
               degrees = seq(0, 337.5, 22.5),
-              # distance from flux tower at 25 m increments
-              radius = seq(25, 150, 25)) %>%
+              # points at 150 m, furthest from flux tower
+              radius = 150) %>%
   # calculate new variables
   mutate(
     # convert degrees to radians
@@ -118,7 +125,8 @@ ggplot() +
   geom_sf(data = ft_75) +
   geom_sf(data = ft_50) +
   geom_sf(data = ft_25) +
-  geom_sf(data = ft_coord) +
+  geom_sf(data = ft_pnt) +
   geom_sf(data = circle_coords, colour = "red") +
+  geom_sf(data = ft_lines, colour = "blue") +
   coord_sf(datum = st_crs(ft_150))
 
